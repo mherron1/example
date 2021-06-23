2851fb4241c97440a89687f41aa958027e2e9950
mherron1 <mherron2021@gmail.com>
2021-06-23 02:59:28 +0100

    lazy loading

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "michael@michaelherron.dev";
 
         // Set the email subject.
         $subject = "New contact from $name";
